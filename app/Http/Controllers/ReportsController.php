e7bb448b5aa436e86f59cb25684abd321765c1d8
Nenad Zivanovic <nenad.zivanovic@adeva.co>
2023-03-19 06:32:57 +0100

    Working on reports

--- app/Http/Controllers/ReportsController.php
+++ app/Http/Controllers/ReportsController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\CashLoanProduct;
+use App\Models\HomeLoanProduct;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Carbon\Carbon;
 
 class ReportsController extends Controller
 {
@@ -13,6 +17,30 @@ class ReportsController extends Controller
      */
     public function index()
     {
-        return view('reports');
+        $adviserId = Auth::user()->id;
+        $cashLoans = CashLoanProduct::where('adviser_id', $adviserId)->get();
+        $homeLoans = HomeLoanProduct::where('adviser_id', $adviserId)->get();
+        $cashLoansData = $this->prepareLoansData($cashLoans, 'Cash loan');
+        $homeLoansData = $this->prepareLoansData($homeLoans, 'Home loan');
+        $loans = array_merge($cashLoansData, $homeLoansData);
+        usort($loans, fn($a, $b) => $a['creation_date'] <= $b['creation_date']);
+
+        return view('reports.list', compact('loans'));
     }
+
+    private function prepareLoansData($loans, $productType)
+    {
+        $returnData = [];
+        if ($loans->count() > 0) {
+            foreach ($loans as $loan) {
+                $returnData[] = [
+                    'product_type' => $productType,
+                    'product_value' => $productType == 'Cash loan' ? $loan->loan_amount : $loan->property_value . ' - ' . $loan->down_payment_amount,
+                    'creation_date' =>  Carbon::createFromFormat('Y-m-d H:i:s', $loan->created_at)->format('Y-m-d')
+                ];
+            }
+        }
+        return $returnData;
+    }
+
 }
---
+++ resources/views/reports/list.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">{{ __('Reports') }}</div>
+                <div class="card-body">
+                    <table class="table table-sm">
+                        <thead>
+                            <tr>
+                                <th>Product type</th>
+                                <th>Product value</th>
+                                <th>Creation date</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($loans as $loan)
+                                <tr>
+                                    <td>{{ $loan['product_type'] }}</td>
+                                    <td>{{ $loan['product_value'] }}</td>
+                                    <td>{{ $loan['creation_date'] }}</td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
